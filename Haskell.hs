f838d70c27606be8000abc80a609063ba42ef06f
mlodyD <maciejdaszkiewicz@tlen.pl>
2024-12-10 14:05:18 +0100

    Update Haskell.hs

--- Haskell.hs
+++ Haskell.hs
@@ -1,3 +1,22 @@
+-- Sprawdza, czy liczba jest palindromem
+isPalindrome :: Int -> Bool
+isPalindrome x = let s = show x in s == reverse s
+
+-- Sprawdza, czy liczba może być zapisana jako suma kwadratów kolejnych liczb naturalnych
+isSumOfSquares :: Int -> Bool
+isSumOfSquares x = not (null (findSquares x))
+
+-- Znajduje wszystkie kombinacje sum kwadratów dla liczby x
+findSquares :: Int -> [[Int]]
+findSquares x = [take n [k..] | k <- [1..maxElem], n <- [1..maxElem], sum (map (^2) (take n [k..])) == x]
+  where
+    maxElem = floor . sqrt . fromIntegral $ x
+
+-- Znajduje wszystkie palindromy mniejsze od n, które spełniają warunek
+findPalindromes :: Int -> [Int]
+findPalindromes n = filter (\x -> isPalindrome x && isSumOfSquares x) [1..(n-1)]
+
+-- Oblicza długość okresu w rozwinięciu dziesiętnym liczby n
 periodLength :: Int -> Int
 periodLength n
     | n `mod` 2 == 0 = periodLength (n `div` 2) 
@@ -9,21 +28,33 @@ periodLength n
         | (10 * reszta) `mod` mianownik == 1 = counter
         | otherwise = findPeriod ((10 * reszta) `mod` mianownik) mianownik (counter + 1)
 
+-- Znajduje liczby z najdłuższym okresem w rozwinięciu dziesiętnym
 longestPeriods :: Int -> [(Int, Int)]
 longestPeriods n =
     let pairs = [(k, periodLength k) | k <- [2..n]]
         maxPeriod = maximum (map snd pairs)
     in filter ((== maxPeriod) . snd) pairs
 
+-- Główna funkcja programu
 main :: IO ()
 main = do
-    putStrLn "----------------------------------"
-    putStrLn "Zadanie 13:"
-    putStrLn "Podaj liczbę n:"
-    input <- getLine
-    let n = read input :: Int
-    let results = longestPeriods n
-    putStrLn $ "Ulamki z najdluzszym okresem: " ++ show (map fst results)
-    putStrLn $ "Najdluzszy okres: " ++ show (snd (head results))
+    -- Zadanie 1
+    putStrLn "Podaj wartość n dla palindromów:"
+    input26 <- getLine
+    let n = read input26 :: Int
+    let palindromes = findPalindromes n
+    putStrLn "Palindromy mniejsze od n, które są sumą kwadratów kolejnych liczb naturalnych:"
+    mapM_ print palindromes
+
+    -- Przerwa wizualna
     putStrLn "----------------------------------"
 
+    -- Zadanie 2
+    putStrLn "Podaj wartość n dla liczb z najdłuższym okresem:"
+    input13 <- getLine
+    let n2 = read input13 :: Int
+    let results = longestPeriods n2
+    putStrLn $ "Liczby z najdłuższym okresem: " ++ show (map fst results)
+    putStrLn $ "Najdłuższy okres: " ++ show (snd (head results))
+    putStrLn "----------------------------------"
+    
