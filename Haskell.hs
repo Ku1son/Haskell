8778c74aab1fadaf725c1e6f8252ef9cfbb1019e
mikiofer <154701434+mikiofer@users.noreply.github.com>
2024-12-11 23:57:47 +0100

    Update Haskell.hs
    zad 35

--- Haskell.hs
+++ Haskell.hs
@@ -1,3 +1,6 @@
+import Data.List (sort,subsequences,nub)
+import Data.List (sortBy)
+import Data.Ord (comparing)
 -- Sprawdza, czy liczba jest palindromem
 isPalindrome :: Int -> Bool
 isPalindrome x = let s = show x in s == reverse s
@@ -35,9 +38,54 @@ longestPeriods n =
         maxPeriod = maximum (map snd pairs)
     in filter ((== maxPeriod) . snd) pairs
 
+
+
+-- Funkcja generująca wszystkie możliwe ciągi cyfr od 0 do m, gdzie każda cyfra może wystąpić maksymalnie p razy,
+-- oraz przynajmniej jeden ciąg musi zawierać cyfrę '1'.
+generateAllSequences :: Int -> Int -> [String]
+generateAllSequences m p =
+    let digits = concatMap (\d -> replicate p d) ['0'..(head (show m))] -- Każda cyfra występuje maksymalnie p razy
+        allSubsequences = subsequences digits                           -- Wszystkie podciągi, dowolnej długości
+    in filter (\s -> elem '1' s) allSubsequences                        -- Usuwamy ciągi bez '1'
+
+-- Funkcja sortująca: najpierw według długości, potem według wartości liczbowej
+sortSequences :: [String] -> [String]
+sortSequences = sortBy ( comparing numericValue <> comparing length)
+
+-- Funkcja obliczająca wartość liczbową ciągu z wiodącymi zerami
+numericValue :: String -> Int
+numericValue = read
+
+  
+-- Funkcja zwracająca podciągi do pozycji n, bez powtórzeń
+subsequencesUpToN :: Int -> Int -> Int -> [String]
+subsequencesUpToN n m p =
+    let allSequences = generateAllSequences m p -- Wszystkie możliwe ciągi spełniające warunek
+        uniqueSequences = nub allSequences      -- Usunięcie powtarzających się podciągów
+        sortedSequences = sortSequences uniqueSequences -- Posortowane najpierw według długości, potem liczbowo
+    in take n sortedSequences                   -- Pobieramy ciągi aż do n-tego
+    
+-- Funkcja zwracająca n-ty ciąg (indeks zaczyna się od 1) z posortowanej listy podciągów
+subsequenceNth :: Int -> Int -> Int -> String
+subsequenceNth n m p =
+    let allSequences = generateAllSequences m p -- Wszystkie możliwe ciągi spełniające warunek
+        uniqueSequences = nub allSequences      -- Usunięcie powtarzających się podciągów
+        sortedSequences = sortSequences uniqueSequences -- Posortowane najpierw według długości, potem liczbowo
+    in sortedSequences !! (n - 2)  -- Zwracamy n-ty ciąg 
+    
 -- Główna funkcja programu
 main :: IO ()
 main = do
+
+    
+    
+    -- Zadanie 35
+    let n = 4
+    let m = 2
+    let p = 2
+    --mapM_ print $ subsequencesUpToN n m p
+    putStrLn $ subsequenceNth n m p
+    
     -- Zadanie 1
     putStrLn "Podaj wartość n dla palindromów:"
     input26 <- getLine
@@ -57,4 +105,6 @@ main = do
     putStrLn $ "Liczby z najdłuższym okresem: " ++ show (map fst results)
     putStrLn $ "Najdłuższy okres: " ++ show (snd (head results))
     putStrLn "----------------------------------"
+    
+    
     
